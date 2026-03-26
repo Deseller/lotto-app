149a571a4d0b00ec713da9c42d9b2ee7ce51d006
Deseller <1004satan@gmail.com>
2026-03-26 14:12:54 +0900

    Create granite.config.ts

---
+++ granite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "@apps-in-toss/web-framework/config";
+
+export default defineConfig({
+  appName: "lotto-app",
+  brand: {
+    displayName: "행운의 로또",
+    primaryColor: "#FF6B35",
+    icon: "https://static.toss.im/appsintoss/24815/9701e3d4-96e0-4967-b22e-52698db9d793.png",
+  },
+  web: {
+    host: "deseller.github.io",
+    port: 443,
+    commands: {
+      dev: "npx serve .",
+      build: "echo done",
+    },
+  },
+});
