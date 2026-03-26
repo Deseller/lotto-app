eebbac8e80e53c4b363ff32cacb439f8528af0be
Deseller <1004satan@gmail.com>
2026-03-26 16:10:55 +0900

    Update granite.config.ts

--- granite.config.ts
+++ granite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@apps-in-toss/web-framework/config";
 export default defineConfig({
   appName: "lucky645",
   brand: {
-    displayName: "행운의 로또",
+    displayName: "행운의 로또645",
     primaryColor: "#FF6B35",
     icon: "https://static.toss.im/appsintoss/24815/9701e3d4-96e0-4967-b22e-52698db9d793.png",
   },
